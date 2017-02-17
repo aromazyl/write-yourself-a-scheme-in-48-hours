2c20bb8f1ac856dd2ea03e2f6a02597d73bd246c
aromazyl <zyl2336709@gmail.com>
2017-02-17 23:17:35 +0800

    update

---
+++ first_chapter/hello.hs
@@ -0,0 +1,16 @@
+#! /usr/bin/env runhugs +l
+--
+-- hello.hs
+-- Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+module Main where
+
+import System.Environment
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn ("Hello, " ++ args !! 0)
+
