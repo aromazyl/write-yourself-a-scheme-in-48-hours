60448256456226080b1d6a9c46023b1b66fb6a26
aromazyl <zyl2336709@gmail.com>
2017-02-18 09:03:53 +0800

    update

--- second_chapter/simple_parse.hs
+++ second_chapter/simple_parse.hs
@@ -10,6 +10,7 @@
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 
 main :: IO ()
 main = do args <- getArgs
@@ -19,6 +20,39 @@ symbol :: Parser Char
 symbol = oneOf "!$%&*+_/:<=?>@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
       Left err -> "No match: " ++ (show err :: String)
       Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+           | List [LispVal]
+           | DottedList [LispVal]
+           | Number Integer
+           | String String
+           | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '“'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseNumber
+        <|> parseString
