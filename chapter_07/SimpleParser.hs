b6c0a9d8bb5a24c70c6fa7a5312fdecea31ecbd2
aromazyl <zyl2336709@gmail.com>
2017-02-22 23:46:33 +0800

    update

--- chapter_07/SimpleParser.hs
+++ chapter_07/SimpleParser.hs
@@ -519,4 +519,28 @@ runIOThrows action = runErrorT (trapError action) >>= return . extractValue
 isBound :: Env -> String -> IO Bool
 isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
 
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do env <- liftIO $ readIORef envRef
+                             maybe (throwError $ UnboundVar "Setting an unbound variable" var)
+                                   (liftIO . (flip writeIORef value))
+                                   (lookup var env)
+                             return value
 
+defineVar :: Env -> String -> LispVal -> ThrowsError  LispVal
+defineVar envRef var value = do
+  alreadyDefined <- liftIO $ isBound envRef var
+  if alreadyDefined
+     then setVar envRef var value >> return value
+     else liftIO $ do
+       valueRef <-  newIORef value
+       env <- readIORef envRef
+       writeIORef envRef ((var, valueRef) : env)
+       return value
+
+bindVars :: Env -> [(String , LispVal)] -> IO Env
+bindVars envRefbindings = readIORef envRef >>= extendEnv
+bindings >>= newIORef
+  where extendEnv bindings env = liftM (++ env ) (mapM
+        addBinding bindings)
+        addBinding (var, value) = do ref <- newIORef value
+                                            return (var, ref)
